682dda58d4ec85f71961da3fd5825ae80400763d
Lucas Lima <lucasViniLima15@gmail.com>
2023-09-25 17:33:27 -0300

    adicionando um controlador

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
